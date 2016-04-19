0336c8e270244f4d6b17ec3a64f4e14a1376c81c
Julien R <julienr@mbp-de-julien-2.home>
2016-04-19 14:36:11 +0200

    Refactorin

--- src/fr/eimonku/anki/paysdumonde/OrthographicProjectionsMaps.java
+++ src/fr/eimonku/anki/paysdumonde/OrthographicProjectionsMaps.java
@@ -18,27 +18,24 @@ public class OrthographicProjectionsMaps {
 	private static final Pattern NAME_PATTERN = Pattern.compile("The ");
 
 	public String mapForDocumentAndEnName(String enName, Document frDocument, Document enDocument) {
-		if ("Malta".equals(enName)) {
-			return "https://en.wikipedia.org/wiki/File:EU-Malta.svg";
-		}
-
-		if (!"Hungary".equals(enName)) {
+		{
 			final String cleanName = NAME_PATTERN.matcher(enName).replaceAll("");
-			final Elements els = this.document
-			    .select(format("div.gallerytext:has(p:contains(%s))", "Morocco".equals(cleanName) ? "Morocco " : cleanName));
-			if (!els.isEmpty()) {
-				return els.get(0).previousElementSibling().select("a").get(0).absUrl("href");
+			final Elements els = this.document.select(format("div.gallerytext:has(p:contains(%s))", cleanName));
+			if (!els.isEmpty() && !"Austro-Hungary".equals(els.first().text())) {
+				return els.first().previousElementSibling().select("a").first().absUrl("href");
 			}
 		}
 
-		for (final String selector : asList("[href*=orthographic]", "[href^=/wiki/Fichier:Location]",
-		    "[href^=/w/index.php?title=Fichier:]")) {
-			for (final Document document : asList(frDocument, enDocument)) {
-				final Elements els = document.select("div.images a" + selector);
-				if (!els.isEmpty()) {
-					return els.get(0).absUrl("href");
-				}
+		for (final Document document : asList(frDocument, enDocument)) {
+			final Elements els = document.select("div.images a[href*=orthographic]");
+			if (!els.isEmpty()) {
+				return els.first().absUrl("href");
 			}
+		}
+
+		final Elements els = frDocument.select("div.images a[href*=Fichier]");
+		if (!els.isEmpty()) {
+			return els.first().absUrl("href");
 		}
 
 		throw new RuntimeException(format("no map for '%s'", enName));
