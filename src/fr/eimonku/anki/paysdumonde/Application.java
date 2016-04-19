87d66b370cf7408e99002e4b43eb835f708af92a
Julien R <julienr@mbp-de-julien-2.home>
2016-04-19 14:49:45 +0200

    Refactoring

--- src/fr/eimonku/anki/paysdumonde/Application.java
+++ src/fr/eimonku/anki/paysdumonde/Application.java
@@ -1,12 +1,24 @@
 package fr.eimonku.anki.paysdumonde;
 
 import static java.lang.System.out;
+import static java.nio.file.Files.createDirectories;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Application {
 	public static void main(String... args) {
-		new ListeDesPaysDuMonde(Paths.get(args[0]), new WikipediaCache(Paths.get(args[1]))).forEach(state -> {
+		final Path mediaDir = Paths.get(args[0]), cacheDir = Paths.get(args[1]);
+		Stream.of(mediaDir, cacheDir).forEach(dir -> {
+			try {
+				createDirectories(dir);
+			} catch (Exception e) {
+				throw new RuntimeException(String.format("unable to create directory '%s'", dir), e);
+			}
+		});
+
+		new ListeDesPaysDuMonde(mediaDir, new WikipediaCache(cacheDir)).forEach(state -> {
 			out.print(state.name);
 			out.print(';');
 			out.print(state.capitals);
