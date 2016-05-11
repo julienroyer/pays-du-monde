f9ad2ada0e16fb1abcf9fb4cd531342ef4ec0656
Julien R <julienr@MacBook-Pro-de-Julien-2.local>
2016-05-11 14:36:31 +0200

    Added TODO

--- src/fr/eimonku/anki/regionsfrancaises/Application.java
+++ src/fr/eimonku/anki/regionsfrancaises/Application.java
@@ -2,6 +2,7 @@ package fr.eimonku.anki.regionsfrancaises;
 
 import static fr.eimonku.anki.AnkiApplication.ankiApplication;
 
+// https://commons.wikimedia.org/wiki/File:France_location_map_–_regions_and_departments_(including_overseas).svg
 public class Application {
 	public static void main(String... args) {
 		ankiApplication(args, (w, cache, resources) -> {
