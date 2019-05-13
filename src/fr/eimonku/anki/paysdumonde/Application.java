ae9d356a9422097b1832c58f5dad7599afdd2fa2
Julien Royer <eldebaran@gmail.com>
2019-05-13 11:31:23 +0200

    cosmetics

--- src/fr/eimonku/anki/paysdumonde/Application.java
+++ src/fr/eimonku/anki/paysdumonde/Application.java
@@ -10,21 +10,21 @@ import java.io.IOException;
 // https://commons.wikimedia.org/wiki/Category:SVG_locator_maps_(location_map_scheme)
 // TODO clean OrthographicProjectionsMaps
 public class Application {
-	public static void main(String... args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		ankiApplication(args, (w, cache, resources) -> {
 			new ListeDesPaysDuMonde(cache, resources).forEach(state -> {
-			  try {
-				  w.append(state.name).append(';');
-				  w.append(state.capitals).append(';');
-				  w.append(state.map).append(';');
-				  w.append(state.flag).append(';');
-				  w.append(state.gentile).append(';');
-				  w.append(state.internetDomain).append(';');
-				  w.append(state.enName).append('\n');
-			  } catch (IOException e) {
-				  throw new RuntimeException("unable to write result", e);
-			  }
-		  });
+				try {
+					w.append(state.name).append(';');
+					w.append(state.capitals).append(';');
+					w.append(state.map).append(';');
+					w.append(state.flag).append(';');
+					w.append(state.gentile).append(';');
+					w.append(state.internetDomain).append(';');
+					w.append(state.enName).append('\n');
+				} catch (IOException e) {
+					throw new RuntimeException("unable to write result", e);
+				}
+			});
 		});
 	}
 }
